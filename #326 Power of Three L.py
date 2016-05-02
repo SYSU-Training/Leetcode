39bbc6c7c80e3ee9d72921a9664068dc217a2a4b
sysulp <sysulp@hotmail.com>
2016-05-02 18:18:32 +0800

    Delete #326 Power of Three L.py

--- #326 Power of Three L.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def isPowerOfThree(self, n):
-        if n<1:
-            return False
-        a=math.log10(n)/math.log10(3)
-        k=a-int(a)
-        return(abs(k)<10e-15)
