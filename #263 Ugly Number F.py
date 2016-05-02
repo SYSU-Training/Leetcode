57ce86e16cacd16ebe6f7238539a6c395018be58
Alan Fan <proidea1992@hotmail.com>
2016-05-02 14:06:52 +0800

    Added files via upload

---
+++ #263 Ugly Number F.py
@@ -0,0 +1,30 @@
+#263. Ugly Number  
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def is_ugly_number(num):
+    prime=list(set(prime_factors(num)))
+    flag=True
+    for i in range(len(prime)):
+        if prime[i] not in prime_list:
+            flag=False
+    return flag
+
+#set
+prime_list=[2,3,5]
+
+#test
+number=raw_input("number: ")
+print is_ugly_number(int(number))
+
