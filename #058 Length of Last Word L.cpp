55dcaad3073ab26eb32046bed02a6701f48298c6
sysulp <sysulp@hotmail.com>
2016-05-04 10:51:42 +0800

    Update #058 Length of Last Word L.cpp

--- #058 Length of Last Word L.cpp
+++ #058 Length of Last Word L.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        if(s.length()==0) return 0;
-        if(s.find(' ')==string::npos) return s.length();
-        else{
-            int len=0;
-            for(int i=s.length()-1;i>=0;i--){
-                if(s[i]!=' ') len++;
-                if(len!=0&&s[i]==' ') return len;
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]!=' ') len++;
+            if(len!=0&&s[i]==' ') return len;
             }
-            return len;
-        }
+        return len;
     }
 };
