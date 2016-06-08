a3ce713a6b4fcd4a8ab3d98b47296d5e6e07d552
Alan Fan <Alan-F@users.noreply.github.com>
2016-06-08 10:02:19 +0800

    Update #002 Add Two Numbers F.py

--- #002 Add Two Numbers F.py
+++ #002 Add Two Numbers F.py
@@ -38,3 +38,32 @@ class Solution(object):
             res3.append(num)
         return res3
         
+
+# Method 2 
+'''
+https://code.csdn.net/snippets/608498#snippets608498
+'''
+class Solution:
+    # @return a ListNode
+    def addTwoNumbers(self, l1, l2):
+        ret = ListNode(0)
+        cur = ret
+
+        sum = 0
+        while True:
+            if l1 != None:
+                sum += l1.val
+                l1 = l1.next
+            if l2 != None:
+                sum += l2.val
+                l2 = l2.next
+
+            cur.val = sum % 10
+            sum /= 10
+            if l1 != None or l2 != None or sum != 0:
+                cur.next = ListNode(0)
+                cur = cur.next
+            else:
+                break
+
+        return ret
